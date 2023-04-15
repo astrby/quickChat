e7355ccf928941dc47e9e5fb6aa7a03fe8da9b44
astrby <anthony.rezu@gmail.com>
2023-04-15 14:22:07 -0600

    Project optimization

--- client/src/components/Join.jsx
+++ client/src/components/Join.jsx
@@ -15,6 +15,7 @@ const Join = () => {
   const navigate = useNavigate();
   const setUsername = localStorage((state)=>state.setUsername);
   const[height, setHeight] = useState('');
+  const[boxWidth, setBoxWidth] = useState('');
   
   const login = (e) =>{
     e.preventDefault();
@@ -29,8 +30,10 @@ const Join = () => {
   useEffect(()=>{
     if(isMobile){
       setHeight('translateY(50%)')
+      setBoxWidth('w-75')
     }else{
       setHeight('translateY(75%)')
+      setBoxWidth('w-50')
     }
   },[])
 
@@ -38,7 +41,7 @@ const Join = () => {
     <>
     {
       <Container className='text-center'>
-        <Card className='w-75 mx-auto' style={{transform: height}}>
+        <Card className = {boxWidth+' mx-auto'} style={{transform: height}}>
           <Form.Label className='mt-2 mx-auto'><img src={logo} style={{width: '4rem', marginRight: '0.5rem'}}/><h3 style={{display: 'inline-block', paddingTop: '1.5rem', color: 'white'}}>QuickChat</h3></Form.Label>
               <Form className='w-100 mx-auto'>
                   <Form.Label className='mt-3'>Ingrese el nombre de usuario</Form.Label>
